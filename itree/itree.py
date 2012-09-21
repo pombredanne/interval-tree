d5228b2898a6e5c46f8e587dec6a7a68d997ff44
Adam DePrince <adeprince@wnyc.org>
2012-09-21 15:08:17 -0400

    Ported to python.

---
+++ itree/itree.py
@@ -0,0 +1,81 @@
+
+def uniq(meth):
+    def _make_unique(*args, **kwargs):
+        history = set()
+        for interval in meth(*args, **kwargs):
+            if (interval.start, interval.stop) in history:
+                continue
+            history.add((interval.start, interval.stop))
+            yield interval
+    return _make_unique
+        
+
+class Tree:
+    def __init__(self, ranges):
+        self.ranges = ranges
+        self.top_node = self.divide_intervals(self.ranges)
+
+    def divide_intervals(self, intervals):
+        if not intervals: return
+        x_center = self.center(intervals)
+        s_center = []
+        s_left = []
+        s_right = []
+
+        for interval in intervals:
+            if interval.start < x_center:
+                s_left.append(interval)
+            elif interval.start > x_center:
+                s_right.append(interval)
+            else:
+                s_center.append(interval)
+        return Node(x_center, s_center, self.divide_intervals(s_left), self.divide_intervals(s_right))
+
+    @staticmethod
+    def sort(seq):
+        return sorted(seq, key=lambda x:(x.start, x.stop))
+
+    def search(self, interval):
+        if isinstance(interval, (tuple, list)):
+            interval = slice(*interval)
+        if isinstance(interval, slice):
+            return Tree.sort(self.search_interval(interval.start, interval.stop))
+        else:
+            return Tree.sort(self.point_search(self.top_node, interval))
+
+    @uniq
+    def search_interval(self, first, last):
+        seen_before = set()
+
+        #TODO: This is disgusting.  Really, stepping forward by one to
+        #make sure we have all of the intermediate intervals?  Whoa,
+        #O(k^n) is way bigger than the O(n lg n) search time implied
+        #by the term "tree".  Okay, fix this in version 0.0.1 maybe?
+
+        for j in range(first, last):
+            for element in self.point_search(self.top_node, j):
+                yield element
+
+    def center(self, intervals):
+        #TODO: A median finder will have much better performance
+        return sorted(intervals, key=lambda x:x.start)[len(intervals)/2].start
+
+    @uniq
+    def point_search(self, node, point):
+        for k in node.s_center:
+            if k.start <= point and point < k.stop:
+                yield k
+        if node.left_node and point < node.left_node.s_max:
+            for k in self.point_search(node.left_node, point):
+                yield k
+        if node.right_node and node.right_node.x_center <= point:
+            for k in self.point_search(node.right_node, point):
+                yield k
+
+class Node:
+    def __init__(self, x_center, s_center, left_node, right_node):
+        self.x_center = x_center
+        self.s_center = list(sorted(s_center, key=lambda x:x.start))
+        self.left_node = left_node
+        self.right_node = right_node
+        self.s_max = max(x.stop for x in self.s_center)
---
+++ tests/itree_test.py
@@ -0,0 +1,83 @@
+from itree.itree import Node, Tree, uniq
+from unittest import TestCase
+
+class IntervalTreeTest(TestCase):
+    def test_center_single_interval(self):
+        self.assertEquals(Tree([]).center([slice(1,5)]),
+                          1)
+
+    def test_center_two_intervals(self):
+        self.assertEquals(Tree([]).center([slice(1,5), slice(2,6)]),
+                          2)
+
+    def test_new(self):
+        self.assertTrue(isinstance(Tree([slice(1,5)]), Tree))
+
+    def test_top_node(self):
+        intervals = [slice(1,5), slice(2,6), slice(3,7)]
+        tree = Tree(intervals)
+        self.assertEquals(tree.top_node.x_center, 2)
+
+    def test_search(self):
+        self.assertIntervals([(1,5)], 3)
+
+    # Not sure I care to support the non-array behaivor
+    # context 'given non-array full-closed "(1..4)" and a point query "3"' do
+    #   it 'returns an array contains a half-open interval (1...5)]' do
+    #     IntervalTree::Tree.new(1..4).search(3).should == [1...5]
+    #   end
+    # end
+
+    def test_search_two_intervals(self):
+        self.assertIntervals([(1,5), (2,6)], 3)
+
+    def test_search_three_intervals(self):
+        self.assertIntervals([(0,8), (1,5), (2,6)],
+                                3)
+
+    def test_search_three_intervals_for_interval(self):
+        self.assertIntervals([(0,8), (1,5), (2,6)],
+                             slice(1,4))
+
+    def test_search_two_intervals_returns_one(self):
+        self.assertIntervals([(1,3), (3,5)], (3,9),
+                             [(3,5)])
+
+    def test_search_three_intervals_return_two(self):
+        self.assertIntervals([(1,3), (3,5), (4,8)],
+                             (3,5),
+                             [(3,5), (4,8)])
+
+    def test_search_three_intervals_return_two(self):
+        self.assertIntervals([(1,3), (3,5), (3,9), (4,8)],
+                             (3,5),
+                             [(3,5), (3,9), (4,8)])
+
+    def assertIntervals(self, intervals, point, returns=None):
+        intervals = map(slice, intervals)
+        if returns is None:
+            returns = intervals
+        else:
+            returns = map(slice, returns)
+        if isinstance(point, (tuple, list)):
+            point = slice(*point)
+        self.assertEquals(Tree(intervals).search(point),
+                          intervals)
+
+
+class UniqueTest:
+    @uniq
+    def test(self):
+        yield slice(1,2)
+        yield slice(2,3)
+        yield slice(1,3)
+        yield slice(1,2)
+        yield slice(3,4)
+
+class TestUniq(TestCase):
+    def test_uniq(self):
+        self.assertEquals(list(UniqueTest().test()),
+                          [slice(1,2),
+                           slice(2,3),
+                           slice(1,3),
+                           slice(3,4)])
